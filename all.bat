5eec3e157c716f0822b56f139000af4e77d95b99
asm128 <pabloarielz@outlook.com>
2019-10-09 02:13:14 -0300

    all.bat

--- all.bat
+++ all.bat
@@ -15,19 +15,53 @@ reason user.json        1
 reason user.json        0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
 reason user.json        1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
 
-season website.json     5 0 
-season website.json     5 1 
-season website.json     5 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
-season website.json     5 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
-season publisher.json   5 0 
-season publisher.json   5 1 
-season publisher.json   5 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
-season publisher.json   5 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
-season company.json     5 0 
-season company.json     5 1 
-season company.json     5 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
-season company.json     5 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
-season user.json        5 0	 
-season user.json        5 1 
-season user.json        5 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
-season user.json        5 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season website.json     256 0 
+season website.json     256 1 
+season website.json     256 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season website.json     256 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season publisher.json   256 0 
+season publisher.json   256 1 
+season publisher.json   256 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season publisher.json   256 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season company.json     256 0 
+season company.json     256 1 
+season company.json     256 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season company.json     256 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season user.json        256 0	 
+season user.json        256 1 
+season user.json        256 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season user.json        256 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+
+season website.json     1024 0 
+season website.json     1024 1 
+season website.json     1024 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season website.json     1024 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season publisher.json   1024 0 
+season publisher.json   1024 1 
+season publisher.json   1024 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season publisher.json   1024 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season company.json     1024 0 
+season company.json     1024 1 
+season company.json     1024 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season company.json     1024 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season user.json        1024 0	 
+season user.json        1024 1 
+season user.json        1024 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season user.json        1024 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+
+season website.json     64 0 
+season website.json     64 1 
+season website.json     64 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season website.json     64 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season publisher.json   64 0 
+season publisher.json   64 1 
+season publisher.json   64 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season publisher.json   64 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season company.json     64 0 
+season company.json     64 1 
+season company.json     64 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season company.json     64 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season user.json        64 0	 
+season user.json        64 1 
+season user.json        64 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season user.json        64 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
